586823d8b1dc9cc060579012f5b8db87baecc86f
Hilde Janie Mjærfoss <hilde.janie.mjaerfoss@nhn.no>
2021-11-17 22:09:01 +0100

    adding in test

--- ElektroniskDodsmelding.Samples/ElektroniskDodsmelding.Samples.Tests/ElektroniskDodsmelding.Samples.Tests/TestResources.cs
+++ ElektroniskDodsmelding.Samples/ElektroniskDodsmelding.Samples.Tests/ElektroniskDodsmelding.Samples.Tests/TestResources.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Hl7.Fhir.Model;
 
 namespace ElektroniskDodsmelding.Samples.Tests
 {
     internal class TestResources
     {
-        internal static PractitionerRole ValidPractitionerRole = new()
+        internal static PractitionerRole ValidPractitionerRole = new ()
         {
             Organization = new ResourceReference(null, "ElektroniskDodsmelding.Samples Test Hospital"),
             Location = new List<ResourceReference>
@@ -18,9 +15,24 @@ namespace ElektroniskDodsmelding.Samples.Tests
             },
             Telecom = new List<ContactPoint>()
             {
-                new(ContactPoint.ContactPointSystem.Email, ContactPoint.ContactPointUse.Work, "t@test.no"),
-                new(ContactPoint.ContactPointSystem.Phone, ContactPoint.ContactPointUse.Work, "007007")
+                new (ContactPoint.ContactPointSystem.Email, ContactPoint.ContactPointUse.Work, "t@test.no"),
+                new (ContactPoint.ContactPointSystem.Phone, ContactPoint.ContactPointUse.Work, "007007")
             }
+        };
+
+        /// <summary>
+        /// Valid person that is alive in Test PREG.
+        /// FNR: 16047508839
+        /// Name: ROBERT STEFANI
+        /// </summary>
+        internal static Patient ValidPatient = new ()
+        {
+            Identifier = new List<Identifier>()
+            {
+                new (Constants.Identifier.FNRSystem, "16047508839")
+            },
+            Deceased = new FhirDateTime(new DateTimeOffset(DateTime.Now.Date)),
+            GeneralPractitioner = null
         };
     }
 }
